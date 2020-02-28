ebcfbdec310a28de603f998bc9ed9b6c1fbd0999
Robert Studtrucker <Studtrucker@ZPID.DE>
2020-02-28 14:58:59 +0100

    rum probiert

--- R/forestPlot.R
+++ R/forestPlot.R
@@ -1,4 +1,3 @@
-# Hello, world!
 #
 # This is an example function named 'hello'
 # which prints 'Hello, world!'.
@@ -13,29 +12,27 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-cumulforest <- function(yi="",vi="",measure="",slab,xlab,xlim,cex, d) {
-
-  par(mar=c(1,0,1,0))
-  dat<-get(d)
-  print(summary(dat))
+cumulforest <- function(yi,vi,measure,d) {
 
   library(metafor)
 
+  #par(mar=c(1,0,1,0))
+  dat<-get(d)
+
+
   # 1. Overall-Effekt und Cumulative forest ####
-  overall_forest <- rma.uni(yi=yi,vi=vi,measure=measure,slab=slab, data=dat)
+  overall_forest <- rma.uni(yi=dat[,yi],vi=dat[,vi],measure=measure,slab=paste(r_author, r_year), data=dat)
 
-  summary<-summary(overall_forest)
+  tmp<-cumul(overall_forest, order=order(dat$r_year),slab=paste(r_author, r_year))
 
-  tmp<-cumul(overall_forest, order=order(dat$r_year),slab=slab)
 
-  f= forest(tmp, xlab=xlab,xlim=xlim,cex=cex)
-  f= abline(v = mean(dat$yi),lty=3,col=2)
+  metafor::forest(tmp,xlab="Response Rates", xlim=c(-1,1.8), cex=1)
+  abline(v = mean(dat$o_g_calc),lty=3,col=2)
 
   invisible();
 
   #yi=o_g_calc,vi=o_g_var_calc,measure="SMD",slab=paste(r_author, r_year),xlab="Response Rates",xlim=c(-1,1.8),cex=1, data="CAMA_Math"
   #ForestPlot(o_g_calc,o_g_var_calc,"SMD",paste(r_author, r_year),"Response Rates",c(-1,1.8),1,'CAMA_Math')
-
 
 }
 
