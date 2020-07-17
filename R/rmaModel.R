be6a194d981769c7a3fa2d93e883050c83469901
Rostu <rs@leibniz-psychology.org>
2020-07-17 17:00:32 +0200

    fixed rmaModel

--- R/rmaModel.R
+++ R/rmaModel.R
@@ -3,10 +3,10 @@ rmaModel <- function(yi,vi,measure,d,pred1=NULL,pred2=NULL) {
   library(metafor)
   library(psych)
   library(jsonlite)
+  library(labelVector)
   dat<-get(d)
   pred1<-unlist(pred1)
   pred2<-unlist(pred2)
-
 
   if( is.null(pred1) && is.null(pred2)){
 
@@ -32,11 +32,10 @@ rmaModel <- function(yi,vi,measure,d,pred1=NULL,pred2=NULL) {
     if(pred1["type"]=="num" && pred2["type"]=="num"){
 
       if(measure == "COR") {
-        somedata<-cbind(label1=scale(dat[,pred1["value"]])[,1],label2=scale(dat[,pred2["value"]])[,1])
-        colnames(somedata)[1]<-pred1["label"]
-        colnames(somedata)[2]<-pred2["label"]
+        mod1<-scale(dat[,pred1["value"]])[,1]
+        mod2<-scale(dat[,pred2["value"]])[,1]
 
-        rma_model <- rma.uni(transf.rtoz(dat[,yi],dat[,o_ni]), transf.rtoz(dat[,vi],dat[,o_ni]),mods=somedata, measure="ZCOR",data=dat)
+        rma_model <- rma.uni(transf.rtoz(dat[,yi],dat[,o_ni]), transf.rtoz(dat[,vi],dat[,o_ni]),mods=~mod1+mod2, measure="ZCOR",data=dat)
 
         ## Transform Z back to Pearson's r and calculation of 95% CI
         #rma_model$yi<-fisherz2r(rma_model$yi)
@@ -47,24 +46,22 @@ rmaModel <- function(yi,vi,measure,d,pred1=NULL,pred2=NULL) {
 
       }else{
 
-        somedata<-cbind(label1=scale(dat[,pred1["value"]])[,1],label2=scale(dat[,pred2["value"]])[,1])
+        mod1<-scale(dat[,pred1["value"]])[,1]
+        mod2<-scale(dat[,pred2["value"]])[,1]
 
-        colnames(somedata)[1]<-pred1["label"]
-        colnames(somedata)[2]<-pred2["label"]
-
-        rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=somedata,measure=measure,data=dat)
+        rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=~mod1+mod2,measure=measure,data=dat)
 
         return(summary(rma_model))
       }
     }else if(pred1["type"]=="cat" && pred2["type"]=="cat"){
 
       if(measure == "COR") {
-        somedata<-cbind(factor(dat[,pred1["value"]]),factor(dat[,pred2["value"]]))
+        mod1<-factor(dat[,pred1["value"]])
+        mod1 <- set_label(mod1,pred1["label"])
+        mod2<-factor(dat[,pred2["value"]])
+        mod2 <- set_label(mod2,pred2["label"])
 
-        colnames(somedata)[1]<-pred1["label"]
-        colnames(somedata)[2]<-pred2["label"]
-
-        rma_model <- rma.uni(transf.rtoz(dat[,yi],dat[,o_ni]), transf.rtoz(dat[,vi],dat[,o_ni]),mods=somedata, measure="ZCOR",data=dat)
+        rma_model <- rma.uni(transf.rtoz(dat[,yi],dat[,o_ni]), transf.rtoz(dat[,vi],dat[,o_ni]),mods=~mod1+mod2, measure="ZCOR",data=dat)
 
         ## Transform Z back to Pearson's r and calculation of 95% CI
         #rma_model$yi<-fisherz2r(rma_model$yi)
@@ -74,10 +71,12 @@ rmaModel <- function(yi,vi,measure,d,pred1=NULL,pred2=NULL) {
         return(paste(print(rma_model),print(theRealModel)))
 
       }else{
-        somedata<-cbind(label1=factor(dat[,pred1["value"]]),label2=factor(dat[,pred2["value"]]))
-        colnames(somedata)[1]<-pred1["label"]
-        colnames(somedata)[2]<-pred2["label"]
-        rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=somedata,measure=measure,data=dat)
+        mod1<-factor(dat[,pred1["value"]])
+        mod1 <- set_label(mod1,pred1["label"])
+        mod2<-factor(dat[,pred2["value"]])
+        mod2 <- set_label(mod2,pred2["label"])
+
+        rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=~mod1+mod2,measure=measure,data=dat)
 
         return(rma_model)
         # return(summary(rma_model))
@@ -93,41 +92,41 @@ rmaModel <- function(yi,vi,measure,d,pred1=NULL,pred2=NULL) {
         numerical<-pred1
       }
       if(measure == "COR") {
-        somedata<-cbind(scale(dat[,numerical["value"]])[,1],factor(dat[,categorical["value"]]))
+        mod1<-scale(dat[,numerical["value"]])[,1]
+        mod2<-factor(dat[,categorical["value"]])
 
-        colnames(somedata)[1]<-numerical["label"]
-        colnames(somedata)[2]<-categorical["label"]
-        rma_model <- rma.uni(transf.rtoz(dat[,yi],dat[,o_ni]), transf.rtoz(dat[,vi],dat[,o_ni]),mods=somedata, measure="ZCOR",data=dat)
-
-        ## Transform Z back to Pearson's r and calculation of 95% CI
-        #rma_model$yi<-fisherz2r(rma_model$yi)
+        rma_model <- rma.uni(transf.rtoz(dat[,yi],dat[,o_ni]), transf.rtoz(dat[,vi],dat[,o_ni]),mods=~mod1+mod2, measure="ZCOR",data=dat)
 
         theRealModel<-predict(rma_model, digits = 3, transf = transf.ztor)
 
         return(paste(print(rma_model),print(theRealModel)))
 
       }else{
-        somedata<-cbind(scale(dat[,numerical["value"]])[,1],factor(dat[,categorical["value"]]))
 
-        colnames(somedata)[1]<-pred1["label"]
-        colnames(somedata)[2]<-pred2["label"]
-        rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=somedata,measure=measure,data=dat)
+        # label1<-pred1["label"]
+        # label2<-pred2["label"]
+        #
+        # df<-data.frame(
+        #   "label1"=scale(dat[,numerical["value"]])[,1],
+        #   "label2"=factor(dat[,categorical["value"]])
+        # )
+        mod1<-scale(dat[,numerical["value"]])[,1]
+        mod2<-factor(dat[,categorical["value"]])
+
+        rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=~mod1+mod2 ,measure=measure,data=dat)
         return(rma_model)
       }
     }
     }else if(!is.null(pred1) && is.null(pred2)){
         #Es gibt einen Prädiktor
         if(pred1["type"]=="cat"){
-          somedata<-cbind(factor(dat[,pred1["value"]]))
-          colnames(somedata)<-pred1["label"]
-          rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=somedata,measure=measure,data=dat)
+          mod<-factor(dat[,pred1["value"]])
+          rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=mod,measure=measure,data=dat)
           return(rma_model)
         }else{
-          somedata<-cbind(scale(dat[,pred1["value"]])[,1])
+          mod<-scale(dat[,pred1["value"]])[,1]
 
-          colnames(somedata)<-pred1["label"]
-
-          rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=somedata,measure=measure,data=dat)
+          rma_model <- rma.uni(yi=dat[,yi],vi=dat[,vi],mods=mod,measure=measure,data=dat)
           return(rma_model)
         }
       }
