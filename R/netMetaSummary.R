d0fb4b15b8a1e2e53702f763e9c376cacb09b9a3
Rostu <rs@leibniz-psychology.org>
2022-05-10 15:10:41 +0200

    further advancements

--- R/netMetaSummary.R
+++ R/netMetaSummary.R
@@ -8,7 +8,8 @@
 #' @author Robert Studtrucker
 #' @export
 netMetaSummary <- function(model) {
-  library(netmeta)
-  requireNamespace("netmeta")
-    netmeta::summary.netmeta(model)
+  #library(netmeta)
+  #requireNamespace("netmeta")
+
+  summary(model)
 }
